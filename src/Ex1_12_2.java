87b3d1dcfb8875f6afe593bbc1a825971ce54ea7
wkn816 <wkn816@gmail.com>
2021-07-20 14:23:11 +0900

    セクション12演習問題。有効な数字の累乗

--- src/Ex1_12_2.java
+++ src/Ex1_12_2.java
@@ -12,21 +12,25 @@
 ----------------------------------------------------*/
 class Ex1_12_2 {
 	public static void main (String[] args) {
-		
+
 		/*コマンドライン引数で受け取った数字の累乗の数のうち、100未満のもののみを表示するプログラムを作る。
 		**【例】コマンドライン引数での入力：3 → 表示される数：3 , 9 , 27 , 81
 		*/
-		
+
 		int receiveNumber = Integer.parseInt(args[0]) ;  //ループ毎にcalcNumberに掛ける数（コマンドライン引数で受け取った値）
 		int calcNumber    = receiveNumber ;              //表示する数（初期値：コマンドライン引数で受け取った値）
-		
+
+		if(receiveNumber == 0 || receiveNumber == 1 || receiveNumber < 0) {
+			System.out.println("適切な値を入力してください");
+		}else {
 		while( calcNumber < 100 ){
-			
+
 			System.out.println("calcNumber：" + calcNumber);
-			
+
 			calcNumber *= receiveNumber ;
-			
+
 		}
-		
+		}
+
 	}
 }
