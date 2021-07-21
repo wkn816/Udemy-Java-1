f21f3fc3e076d5fc302b13342edea2813182c8ed
wkn816 <wkn816@gmail.com>
2021-07-21 11:35:20 +0900

    自販機投入金額のお釣りの計算

--- src/Ex1_1_1.java
+++ src/Ex1_1_1.java
@@ -1,7 +1,7 @@
 class Ex1_1_1 {
 	public static void main (String[] args) {
 
-		int coinNum = args.length ;          //投入された硬貨の枚数
+		int coinNum = args.length -1 ;          //投入された硬貨の枚数
 		int coinChk = 0 ;                    //チェック対象の硬貨の金額
 		int coinSum = 0 ;                    //投入された硬貨の合計金額 ※使用可能な硬貨のみで算出
 
@@ -17,7 +17,12 @@ class Ex1_1_1 {
 			System.out.println("警告：" + coinChk + "は硬貨として適切な値ではありません");
 		}
 	}
+
+	//お釣りの計算（合計金額ー商品の値段）
+	int price = Integer.parseInt(args[args.length -1]);
+	int change = coinSum - price ;
 	System.out.println("ただいまの投入金額は" + coinSum + "円です");
+	System.out.println("お釣りは" + change + "円ですりがとう");
 
 
 	}
