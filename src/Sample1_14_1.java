65fdad22600d67f16512a14cef20bc92cdfec266
wkn816 <wkn816@gmail.com>
2021-07-22 21:32:10 +0900

    セクション14クラスメソッドのソースコード

---
+++ src/Sample1_14_1.java
@@ -0,0 +1,21 @@
+class Sample1_14_1 {
+	
+	public static void main(String[] args) {
+		
+		//メソッドのしくみ
+		
+		int print = sum( 2 , 3 ) ;
+		
+		System.out.println("print：" + print);
+		
+	}
+	
+	static int sum( int num1 , int num2 ) {
+		
+		int calcResult = num1 + num2 ;
+		
+		return calcResult ;
+		
+	}
+	
+}
